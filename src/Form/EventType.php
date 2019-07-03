0082ef9caccfd2e919f5b08a2b0aed0017aa8732
Paillard Dimitri <dimi531p@gmail.com>
2019-07-03 22:26:49 +0200

    Résolution d'un bug

---
+++ src/Form/EventType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('pictureFile', FileType::class, [
+                'mapped' => false,
+                'required' => false
+            ])
+            ->add('city')
+            ->add('description', TextareaType::class)
+            ->add('dateStart', DateType::class)
+            ->add('dateEnd', DateType::class)
+            ->add('url')
+            ->add('price')
+            ->add('language');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
